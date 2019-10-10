fd6217104243e57d88eb6e74588558c1b649fa30
stivb <stivbennett@gmail.com>
2019-10-10 15:39:06 +0100

    first version of score calculator done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  value='0';
+  total = 0;
   constructor() {}
+  btnCaps = [
+    [7, 8, 9],
+    [4, 5, 6],
+    [1, 2, 3],
+    [0, '@15','@30']
+  ];
+
+  onButtonPress(symbol) {
+    console.log(symbol);
+    this.value=symbol;
+  }
 
 }
