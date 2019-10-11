270c1866e89646fc19737a872c27f44bb3362319
stivb <stivbennett@gmail.com>
2019-10-11 17:24:26 +0100

    pretty much done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,42 +6,96 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  creditScores:creditScore[] = new Array();
+  
+  
+  creditScores:creditScore[];
   i:number;
-  twoDigitString = '00'
-  twoDigitValue = parseInt(this.twoDigitString)
-  value=0;
-  total = 0;
-  constructor() {
-    
-    for (this.i=0;this.i<8;this.i++)
-    {
-      this.creditScores.push(new creditScore());
-    }
+  twoDigitString:string;
+  twoDigitValue:number;
+  value:number;;
+  cumulativeValue:number;
+  cumulativeCredits:number;
+  total:number;;
+  studentTotal:string;
+  
+  reset()
+  {
+  this.creditScores = new Array();
+  this.twoDigitString = '00';
+  this.twoDigitValue = parseInt(this.twoDigitString)
+  this.value=0;
+  this.cumulativeValue=0;
+  this.cumulativeCredits=0.0;
+  this.total = 0;
+  this.studentTotal='';
   }
+
+  constructor() {
+    this.reset();
+    
+  }
+
   btnCaps = [
     [7, 8, 9],
     [4, 5, 6],
     [1, 2, 3],
     [0, '@15','@30']
   ];
+  
+
 
  
   
-
   onButtonPress(symbol) {
-    if (symbol=='@15'||symbol=='@30') this.twoDigitString = '00';
-    else this.twoDigitString = this.twoDigitString.charAt(1) + symbol;    
-    this.value = parseInt(this.twoDigitString);
+    
+    var nc:number;
+    var modTotal:number;
+    if (symbol=='@15'||symbol=='@30') 
+    {
+      nc = symbol.substring(1)*1;
+      this.creditScores.push(new creditScore(nc,this.value));
+      this.twoDigitString = '00';
+      this.calculateTotal();
 
+    }
+    else 
+    {
+      this.twoDigitString = this.twoDigitString.charAt(1) + symbol;    
+      this.value = parseInt(this.twoDigitString);
+    }
+    
+  }
+
+
+  calculateTotal()
+  {
+    var summation:string = "";
+    var cumulativeValue=0;
+    var cumulativeCredits=0;
+    for (let cs of this.creditScores)
+    {
+        summation+=cs.toString();
+        cumulativeCredits+=cs.numCredits;
+        cumulativeValue+=cs.total;
+    }
+    this.studentTotal=summation;
+    this.cumulativeCredits = cumulativeCredits;
+    console.log(this.cumulativeCredits.toFixed(2));
+    if (cumulativeCredits!=0) this.cumulativeValue=cumulativeValue/cumulativeCredits;
   }
 
 }
 
 class creditScore {
-  numCredits=15
-  score=0
-  total=this.numCredits*this.score;
+  numCredits=15;
+  score=0;
+  total=0;
+
+  constructor(nc,s) {
+    this.score=s;
+    this.numCredits=nc;
+    this.total=this.numCredits*this.score;
+  }
   setScore(s: number) 
   {
     this.score=s;
@@ -51,5 +105,10 @@ class creditScore {
   {
     this.numCredits=nc;
     this.total=this.numCredits*this.score;
+  }
+
+  toString()
+  {
+    return "" + this.numCredits + "*" +this.score + "=" + this.total + "\n";
   }
 }
